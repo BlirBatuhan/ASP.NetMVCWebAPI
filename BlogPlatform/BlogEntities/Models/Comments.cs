eaf5d3c7236a2d4500c86a7521a0f6f0c66defc1
Batuhan Bilir <114738657+BlirBatuhan@users.noreply.github.com>
2023-12-06 20:34:59 +0300

    delete
    delete

--- BlogPlatform/BlogEntities/Models/Comments.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogEntities.Models;
-
-public class Comments
-{
-    public int CommentId { get; set; }
-    public string Content { get; set; }
-    public DateTime DatePosted { get; set; }
-    public Users Author { get; set; }
-}
